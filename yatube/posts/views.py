90a908a26ce7a1381f212a249078c9fa1dbc9c99
nu-shtosh <i9858955749@ya.ru>
2021-08-09 16:36:23 +0300

    dop proverka

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
-
 from posts.forms import CommentForm, PostForm
 from posts.models import Follow, Group, Post, User
+
 from yatube.settings import PAGE_COUNT
 
 
@@ -118,14 +118,14 @@ def add_comment(request, username, post_id):
 def page_not_found(request, exception):
     return render(
         request,
-        "misc/404.html",
-        {"path": request.path},
+        'misc/404.html',
+        {'path': request.path},
         status=404
     )
 
 
 def server_error(request):
-    return render(request, "misc/500.html", status=500)
+    return render(request, 'misc/500.html', status=500)
 
 
 @login_required
